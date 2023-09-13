574b6d1a70e5afdb5cb629446b341d2c78954733
Andre Velez <andreVelez151@hotmail.com>
2023-09-13 10:28:05 -0500

    Agregar .gitignore, y primero archivos

---
+++ script.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector('h1');
+
+const input1 = document.getElementById('calculo1');
+const input2 = document.getElementById('calculo2');
+const btn = document.querySelector('#btnCalcular');
+
+function miBoton(){
+    console.log(input1.value + input2.value);
+}
+
